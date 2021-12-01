2d68ff015d9c2e732ef9e9e84271104286c06bfa
Vanush27 <vanushgrigoryan90gmail.com>
2021-12-01 19:21:59 +0400

    lesson8Collections

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArrayController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\ValidationController;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +23,27 @@ Route::get('/', function () {
 });
 
 
-
 Route::get('/post', [ArrayController::class, 'index']);
 Route::get('validation', [ValidationController::class, 'showform']);
 Route::post('validation', [ValidationController::class, 'validateform']);
 Route::get('/car', [CarController::class, 'car']);
 
+
+Route::get('/cache_set', function () {
+    Cache::put('cachekey', 'I am in the cache!', 60);
+});
+
+Route::get('/cache_get', function () {
+    return Cache::get('cachekey', 'default value');
+});
+
+Route::get('/collect1', [CollectionController::class, 'collection_class']);
+Route::get('/collect2', [CollectionController::class, 'collect_method']);
+Route::get('/src_collection', [CollectionController::class, 'search_data']);
+Route::get('/filter_collection', [CollectionController::class, 'filter_data']);
+Route::get('/sort_collection', [CollectionController::class, 'sort_data']);
+Route::get('key_collection', [CollectionController::class, 'read_keys']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
